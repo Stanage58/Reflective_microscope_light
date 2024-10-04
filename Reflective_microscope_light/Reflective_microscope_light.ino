a03088e806c9bf2c79c15f31f5d4dcb495530489
Thomas Lester <t.lester@ucl.ac.uk>
2024-10-04 11:47:40 +0100

    Added potentiometer control so that light change only responds if different by 5

--- Reflective_microscope_light/Reflective_microscope_light.ino
+++ Reflective_microscope_light/Reflective_microscope_light.ino
@@ -1,18 +1,23 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
+  #include <Adafruit_NeoPixel.h>
+  #ifdef __AVR__
+  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+  #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN    6
+  // Which pin on the Arduino is connected to the NeoPixels?
+  // On a Trinket or Gemma we suggest changing this to 1:
+  #define LED_PIN    6
 
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 24
+  // How many NeoPixels are attached to the Arduino?
+  #define LED_COUNT 24
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+  Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-uint32_t white = strip.Color(246, 231, 210);
+  uint32_t white = strip.Color(246, 231, 210);
+
+  int sensor_pin = A0;
+  int current_sensor_value;
+  int previous_sensor_value;
+  int brightness_value;
 
 void setup() {
 
@@ -20,7 +25,7 @@ void setup() {
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-
+  Serial.begin(9600);
 
   // put your setup code here, to run once:
 
@@ -29,6 +34,45 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
- strip.fill(white);
- strip.show();
-}
+  current_sensor_value = analogRead(sensor_pin);
+
+  if (current_sensor_value - previous_sensor_value > 5 ) {
+      brightness_value = current_sensor_value;
+      //strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+      Serial.print("POS CHANGE");
+      Serial.println(current_sensor_value);
+      previous_sensor_value = current_sensor_value;
+      delay(50);
+  } else {
+    if (previous_sensor_value - current_sensor_value > 5 ) {
+      brightness_value = current_sensor_value;
+      //strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+      Serial.print("NEG CHANGE");
+      Serial.println(current_sensor_value);
+      previous_sensor_value = current_sensor_value;
+      delay(50);
+    } else {
+         Serial.println(current_sensor_value);
+         delay(50);
+  }
+  }
+
+
+  }
+
+ 
+  //strip.fill(white);
+ // strip.show();
+
+
+
+
+
+
+
+
+
+
+
+
+
